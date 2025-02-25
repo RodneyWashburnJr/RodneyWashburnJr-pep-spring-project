2e4b8cbe72f2548e0a9f9b7205421638f5fde0aa
Rodney Washburn <Rodneywashburn01@gmail.com>
2025-02-25 01:46:57 +0000

    Spring Social Media pt3

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -3,8 +3,11 @@ package com.example.repository;
 import com.example.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    Optional<Message> findMessageById (Integer messageId);
+    List<Message> findByPostedBy (Integer postedBy);
 }
